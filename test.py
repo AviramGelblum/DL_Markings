d4c584d1da57a6279c01ce67644ca99070b9c6d6
AviramGelblum <avgelb@gmail.com>
2021-07-12 18:42:53 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import evaluation
-import numpy as np
-
-labels = np.array([[0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1],
-                   [0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1],
-                   [1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1]])
-stride = 3
-final_labels = evaluation.final_sequence_from_labeled_windows(labels, stride)
